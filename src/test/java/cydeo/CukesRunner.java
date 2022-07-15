679460a099b870c747eeefb79b9f4bceffbd4fd0
rameshwar lekhak <lekhakram@gmail.com>
2022-07-15 01:44:26 -0400

    my First commit

---
+++ src/test/java/cydeo/CukesRunner.java
@@ -0,0 +1,21 @@
+package cydeo;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
+        features = "src/test/java/cydeo/resources/My_Jenkin.feature",
+
+        publish = true
+)
+
+public class CukesRunner {
+
+}
